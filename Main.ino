62b59b7060c259fbde70fa25bee1d689843d581b
EeshanAmdekar07 <eeshanxxxx@gmail.com>
2026-03-10 09:37:48 +0530

    Final Commit
    Updated the code to include MQTT functionality for motion detection and environmental data publishing.

--- Main.ino
+++ Main.ino
@@ -1,116 +1,155 @@
 #include <Servo.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 #include "DFRobot_BME280.h"
 #include <Wire.h>
 
-const char* ssid = "NAME";
-const char* password = "PASSWORD";
+const char* ssid="XYZ";
+const char* password="45362573";
+const char* mqtt_server="192.168.29.151";
+const char* topic_motion="farm/motion";
+const char* topic_env="farm/environment";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 DFRobot_BME280 bme(0x77);
 
 Servo pan;
 Servo tilt;
 
-int pirState1 = LOW;
-int pirState2 = LOW;
-int pirState3 = LOW;
+int pirState1=LOW;
+int pirState2=LOW;
+int pirState3=LOW;
 
-void setup_wifi();
-void motionDetect();
-void motormove(int posPan, int posTilt);
-void bme280();
+int pir1=6;
+int pir2=7;
+int pir3=8;
 
 void setup()
 {
-  Serial.begin(115200);
-
-  Wire.setSCL(19);
+	Serial.begin(115200);
+	Wire.setSCL(19);
   Wire.setSDA(18);
-
+  Wire.begin();
   bme.begin();
 
-  pan.attach(37);
-  tilt.attach(38);
-  pan.write(0);
-  tilt.write(0);
+	pan.attach(37);
+	tilt.attach(38);
+	pan.write(90);
+	tilt.write(90);
 
-  pinMode(6, INPUT);
-  pinMode(7, INPUT);
-  pinMode(8, INPUT);
+	pinMode(pir1,INPUT);
+	pinMode(pir2,INPUT);
+	pinMode(pir3,INPUT);
 
-  setup_wifi();
-
-  Serial.println("Warming up PIR sensor...");
-  delay(45000);
-  Serial.println("Ready to detect motion!");
+	setup_wifi();
+	client.setServer(mqtt_server,1883);
+  
+	Serial.println("Warming up PIR sensors...");
+	delay(45000);
+	Serial.println("Ready to detect motion!");
 }
 
 void loop()
 {
-  bme280();
-  motionDetect();
+	if(!client.connected())
+	{
+		reconnect();
+	}
+	client.loop();
+
+	bme280_read();
+	motionDetect();
 }
 
 void setup_wifi()
 {
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-  WiFi.status() == WL_CONNECTED);
-  Serial.println("WiFi Connected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+	Serial.println("Connecting to WiFi...");
+	WiFi.begin(ssid,password);
+	while(WiFi.status()!=WL_CONNECTED)
+	{
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("\nWiFi Connected!");
+	Serial.println(WiFi.localIP());
+}
+
+void reconnect()
+{
+	while(!client.connected())
+	{
+		Serial.println("Connecting to MQTT...");
+		if(client.connect("ESP32_PIR"))
+		{
+			Serial.println("MQTT Connected");
+		}
+		else
+		{
+			Serial.println("MQTT Failed. Retrying...");
+			delay(2000);
+		}
+	}
 }
 
 void motionDetect()
 {
-  int val1 = digitalRead(6);
-  int val2 = digitalRead(7);
-  int val3 = digitalRead(8);
-
-  if (val1 == HIGH)
-  {
-    Serial.println("Motion detected (PIR 1)");
-    pirState1 = val1;
-    motormove(90, 90);
-  }
-  else if (val2 == HIGH)
-  {
-    Serial.println("Motion detected (PIR 2)");
-    pirState2 = val2;
-    motormove(90, 90);
-  }
-  else if (val3 == HIGH)
-  {
-    Serial.println("Motion detected (PIR 3)");
-    pirState3 = val3;
-    motormove(90, 90);
-  }
-  else
-  {
-    Serial.println("No motion detected!");
-  }
+	int val1=digitalRead(pir1);
+	int val2=digitalRead(pir2);
+	int val3=digitalRead(pir3);
+	if(val1==HIGH&&pirState1==LOW)
+	{
+		Serial.println("Motion detected (PIR 1)");
+		client.publish(topic_motion,"PIR1 MOTION");
+		pirState1=HIGH;
+		motormove(45,90);
+	}
+	else if(val2==HIGH&&pirState2==LOW)
+	{
+		Serial.println("Motion detected (PIR 2)");
+		client.publish(topic_motion,"PIR2 MOTION");
+		pirState2=HIGH;
+		motormove(90,90);
+	}
+	else if(val3==HIGH&&pirState3==LOW)
+	{
+		Serial.println("Motion detected (PIR 3)");
+		client.publish(topic_motion,"PIR3 MOTION");
+		pirState3=HIGH;
+		motormove(135,90);
+	}
+	if(val1==LOW)pirState1=LOW;
+	if(val2==LOW)pirState2=LOW;
+	if(val3==LOW)pirState3=LOW;
 }
 
-void motormove(int posPan, int posTilt)
+void motormove(int posPan,int posTilt)
 {
-  pan.write(posPan);
-  tilt.write(posTilt);
-  delay(500);
+	pan.write(posPan);
+	tilt.write(posTilt);
+	delay(500);
 }
 
-void bme280()
+void bme280_read()
 {
-  Serial.print("Temp: ");
-  Serial.print(bme.getTemperature() / 100.0);
-  Serial.print(" °C  ");
+	float temp=bme.getTemperature();
+	float pressure=bme.getPressure();
+	float humidity=bme.getHumidity();
+  
+	Serial.print("Temp: ");
+	Serial.print(temp);
+	Serial.print(" C ");
 
-  Serial.print("Pressure: ");
-  Serial.print(bme.getPressure() / 100.0);
-  Serial.print(" hPa  ");
+	Serial.print("Pressure: ");
+	Serial.print(pressure);
+	Serial.print(" hPa ");
 
-  Serial.print("Humidity: ");
-  Serial.print(bme.getHumidity() / 1000.0);
-  Serial.println(" %");
+	Serial.print("Humidity: ");
+	Serial.print(humidity);
+	Serial.println(" %");
 
-  delay(500);
+	String payload=String(temp)+","+String(pressure)+","+String(humidity);
+	client.publish(topic_env,payload.c_str());
+	delay(500);
 }
